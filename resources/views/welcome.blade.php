483c5b15034b8e6d1151c280282c6b9a128a2759
peng <531299585@qq.com>
2017-10-18 13:44:29 +0000

    测试一下

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    更新一下试试看
                 </div>
 
                 <div class="links">
